ecbfed968e42e197d2a80b57ff9bb8c6bb2cae4c
Noah Velasco <noahvelasco299@gmail.com>
2022-09-19 19:46:12 -0600

    init db commit

---
+++ cbv_team4/backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors'); //helps get outside packages from express
+const mongoose = require('mongoose'); //helps connect to mongodb 
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//set up middleware 
+app.use(cors());
+app.use(express.json()); //helps pars JSON
+
+//database uri - retrieved from the mongodb 
+const uri = process.env.COMPASS_URI;
+// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+  mongoose.connect(uri, {});
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+//import them basically
+const usersRouter = require('./routes/users');
+
+//use those files ------- THIS IS DOING THE CRUD OPERATION
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
