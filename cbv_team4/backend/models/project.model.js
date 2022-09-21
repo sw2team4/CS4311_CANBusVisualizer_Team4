f25c82c07a5f47b27ab873ce179f5e3a66753313
aazambrano2 <aazambrano2@miners.utep.edu>
2022-09-20 22:15:35 -0400

    added project schema and project route. Updated server.js

---
+++ cbv_team4/backend/models/project.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
+  //schema for a project
+    name: { 
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 1
+  },  
+  analyst_initals: { type: String, required: true },
+  event_name: { type: String, required: true },
+  event_date: { type: Date, required: true },
+  can_id: { type: Number, required: true },
+  vehicle_id: { type: String, required: true },
+  baud_rate: { type: Number, required: true },
+  dbc_file_name: { type: String, required: true },
+  off_limits_file_name: { type: String, required: false},
+}, {
+  timestamps: true,
+});
+
+const Project = mongoose.model('Project', projectSchema);
+
+module.exports = Project;
