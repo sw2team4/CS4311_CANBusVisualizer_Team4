96f69fd09fe99787c6096f061707360815fe92f1
antonioaguirre <180500Am>
2022-10-05 23:42:20 -0600

    Added Popups. CAN Map was not responsive and overlapped nodes

---
+++ cbv_team4/src/components/Displayer/Popups/AnnotatePopup.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+// import Form from 'react-bootstrap/Form';
+import './savePopup.css'
+ 
+
+
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      backdrop="static"
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Annotate Packets
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <textarea></textarea>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+function AnnotatePopup() {
+  const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <>
+      <Button className='button-color' onClick={() => setModalShow(true)}>
+      Annotate Project
+      </Button>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+  );
+}
+
+
+
+
+export default AnnotatePopup;
