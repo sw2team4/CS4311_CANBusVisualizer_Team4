d434ef8747b2c108a6ead1cc43067581105ad54b
Noah <noahvelasco299@gmail.com>
2022-10-21 14:16:53 -0400

    update process to run the entire application

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,26 +1,48 @@
 #!/bin/bash
 
 
-apt-get install mongodb
-echo " "
-apt install nodejs
-echo " "
-apt install npm
-echo " "
-npm install express cors mongoose dotenv
-echo " "
-npm install -g nodemon
-echo " "
-npm install react-scripts
-echo " "
-npm install react-router-dom
-echo " "
-npm install axios
-echo " "
-npm install react-datepicker
-echo " "
-npm install socketcan
 
-#npm audit fix --force
-#npm audit fix --force
-#npm audit fix --force
+echo ">>>>>>>>>>>>>>>>>> Downloading React Dependencies Now <<<<<<<<<<<<<<<<<<<<<<<<"
+
+#sudo apt-get install mongodb
+echo " "
+sudo apt install nodejs
+echo " "
+sudo apt install npm
+echo " "
+#sudo npm install express cors mongoose dotenv
+echo " "
+sudo npm install -g nodemon
+echo " "
+sudo npm install react-scripts
+echo " "
+sudo npm install react-router-dom
+echo " "
+#sudo npm install axios
+echo " "
+sudo npm install react-datepicker
+echo " "
+#npm install socketcan
+echo " "
+sudo npm install reactflow
+echo " "
+
+echo ">>>>>>>>>>>>>>>>>> Downloading Flask Dependencies Now <<<<<<<<<<<<<<<<<<<<<<<<"
+
+echo " "
+sudo apt-get install python3
+echo " "
+sudo npm install react-bootstrap bootstrap
+echo " "
+pip install -U flask-cors
+echo " "
+pip install Flask
+echo " "
+pip install flask python-dotenv
+echo " "
+pip install Flask-PyMongo
+echo " "
+
+#Remove lines below if causing problems then run ./dependencies.sh again
+#sudo npm audit fix --force #take out this line if can't render
+#sudo npm audit fix --force #take out this line if can't render
