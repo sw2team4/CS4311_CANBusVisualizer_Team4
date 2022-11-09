e0b19f474e3dce22075d8f7ccfcff18ac0f968bb
aazambrano2 <aazambrano2@miners.utep.edu>
2022-11-09 01:54:52 -0500

    updated sort functions for highest id and lowest id

--- cbv_team4/api/packet_container.py
+++ cbv_team4/api/packet_container.py
@@ -53,11 +53,81 @@ class Packet_Container(object):
     
     '''
     Description
+    Ref: https://www.geeksforgeeks.org/python-program-for-bubble-sort/
     '''
-    #TODO Implement with bubbe sort for now.
-    def sort_saved(self, sort_by: int):
-        s = []
-        pass
+    #TODO Implement with bubble sort for now. 
+    #Question,should we sort the saved packets array? Or Give a temporary saved packet array to use to display in table?
+    #Sort by highest ID,Lowest ID, Oldest ID or Most recent
+    #We need to moduralize this method into smaller method by types of sorts instead of the sort_by method
+    #This is textbook high cohesion...
+    def sort_saved_by_highest_id(self, sort_by: int):
+        s = [i for i in self.saved]
+        n = len(self.saved)
+        # optimize code, so if the array is already sorted, it doesn't need
+        # to go through the entire process
+        swapped = False
+        # Traverse through all array elements
+        for i in range(n-1):
+            # range(n) also work but outer loop will
+            # repeat one time more than needed.
+            # Last i elements are already in place
+            for j in range(0, n-i-1):
+
+                # traverse the array from 0 to n-i-1
+                # Swap if the element found is greater
+                # than the next element
+                if s[j] > s[j + 1]:
+                    swapped = True
+                    s[j], s[j + 1] = s[j + 1], s[j]
+        
+            if not swapped:
+                # if we haven't needed to make a single swap, we
+                # can just exit the main loop.
+                return 'not swapped'
+            else:
+                #give a temporary saved packets list to view when the user chooses this option
+                temporary_saved_packets = dict()
+                for key in s:
+                    temporary_saved_packets[key] = self.session[key]
+                return temporary_saved_packets
+
+    #This is logical cohesion...but it is what it is
+    def sort_saved_by_lowest_id(self):
+        s = [i for i in self.saved]
+        n = len(self.saved)
+        # optimize code, so if the array is already sorted, it doesn't need
+        # to go through the entire process
+        swapped = False
+        # Traverse through all array elements
+        for i in range(n-1):
+            # range(n) also work but outer loop will
+            # repeat one time more than needed.
+            # Last i elements are already in place
+            for j in range(0, n-i-1):
+
+                # traverse the array from 0 to n-i-1
+                # Swap if the element found is less than or equal
+                # than the next element
+                if s[j] <= s[j + 1]:
+                    swapped = True
+                    s[j], s[j + 1] = s[j + 1], s[j]
+        
+            if not swapped:
+                # if we haven't needed to make a single swap, we
+                # can just exit the main loop.
+                return 'not swapped'
+            else:
+                #give a temporary saved packets list to view when the user chooses this option
+                temporary_saved_packets = dict()
+                for key in s:
+                    temporary_saved_packets[key] = self.session[key]
+                return temporary_saved_packets
+
+    def sort_saved_by_oldest_id(self):
+        pass 
+    def sort_saved_by_newest_id(self):
+        pass   
+
     #TODO Implement with bubbe sort for now.
     def sort_filter(self, sort_by: int):
         pass
