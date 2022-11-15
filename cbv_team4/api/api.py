00dd6c2962a591617b94e9f7b8a2f31673badda9
Emiliano <Emiliano-R-D-L-C@outlook.com>
2022-11-15 15:29:35 -0500

    Added blueprint to api

--- cbv_team4/api/api.py
+++ cbv_team4/api/api.py
@@ -5,11 +5,13 @@ from flask_cors import CORS
 from packet_receiver import packet_receiver
 from project_configuration import project_configuration
 from node_manager import node_manager
+from syncronizer import syncronizer
 
 # Main app and the other files that contain the other routes
 app = Flask(__name__)
 app.register_blueprint(packet_receiver)
 app.register_blueprint(project_configuration)
+app.register_blueprint(syncronizer)
 # app.register_blueprint(node_manager)
 CORS(app)
 #TODO: make sure backend is killed with scripts
