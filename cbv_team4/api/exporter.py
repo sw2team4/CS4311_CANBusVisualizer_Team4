225dfc416f1f566a927afbb4c0901585127124f3
Emiliano <Emiliano-R-D-L-C@outlook.com>
2022-11-17 12:00:53 -0500

    Added signatures for XML and JSON

--- cbv_team4/api/exporter.py
+++ cbv_team4/api/exporter.py
@@ -15,6 +15,8 @@ projects = db.projects
 nodes = db.nodes
 packets = db.packets
 
+#-----------------CSV------------------------------------
+
 @exporter.route('/export/packetCSV', methods=["POST", "GET"])
 def packetCSV():
     packets_df = pd.DataFrame(list(packets.find()))
@@ -38,4 +40,32 @@ def projectCSV():
     project_df.to_csv("project.csv")
 
     return "project to csv"
+
+#---------------JSON-------------------------------
+
+@exporter.route('/export/packetJSON', methods=["POST", "GET"])
+def packetJSON():
+    return "Packets to JSON"
+
+@exporter.route('/export/nodesJSON', methods=["POST", "GET"])
+def nodeJSON():
+    return "Nodes to JSON"
+
+@exporter.route('/export/projectJSON', methods=["POST", "GET"])
+def projectJSON():
+    return "project to JSON"
+
+#-----------------XML-----------------------------
+
+@exporter.route('/export/packetXML', methods=["POST", "GET"])
+def packetXML():
+    return "Packets to XML"
+
+@exporter.route('/export/nodesXML', methods=["POST", "GET"])
+def nodeXML():
+    return "Nodes to XML"
+
+@exporter.route('/export/projectXML', methods=["POST", "GET"])
+def projectXML():
+    return "project to XML"
 
